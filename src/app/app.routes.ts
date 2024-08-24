2bd57afcb3bd99836f04c14254f22da30aad3e8e
Otmane <samdev.br@gmail.com>
2024-08-24 16:16:03 +0100

    setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent:() => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'login',
+        loadComponent:() => import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'dashboard',
+        loadComponent:() => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+        
+];
